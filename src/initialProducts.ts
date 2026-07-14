92da95a376e16261be689d1fb8798715fb405c66
DaliTesfaye <112868614+DaliTesfaye@users.noreply.github.com>
2026-07-14 01:23:13 +0100

    images update

--- src/initialProducts.ts
+++ src/initialProducts.ts
@@ -10,7 +10,7 @@ export const INITIAL_PRODUCTS: Omit<Product, "id">[] = [
     active: true,
     probability: 7.5,
   },
-  
+
   {
     uniqueKey: "2",
     name: "Friteuse",
@@ -37,23 +37,25 @@ export const INITIAL_PRODUCTS: Omit<Product, "id">[] = [
     remaining: 999999,
     active: true,
   },
-    {
+
+  {
     uniqueKey: "5",
     name: "Produit Mazraa",
-    image: "/images/products/chiken-nuggets.png",
+    probability: 1,
+    image: "/images/products/4.png",
     remaining: 15,
     active: true,
-    probability: 40,
   },
   {
     uniqueKey: "6",
-    name: "Produit Mazraa",
-    probability: 1,
-    image: "/images/products/crousti.png",
-    remaining: 15,
+    name: "A la Prochaine",
+    probability: 40,
+    image:
+      "https://imgs.search.brave.com/nsiGlyISticIptpic58hVto5HUTem6f76y5MqA_t5bE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi9mYWls/ZWQtcmVkLXJ1YmJl/ci1zdGFtcC1vdmVy/LXdoaXRlLWJhY2tn/cm91bmQtODg0MTIz/MTQuanBn",
+    remaining: 999999,
     active: true,
   },
-      {
+  {
     uniqueKey: "7",
     probability: 2.8,
     name: "Tablier",
@@ -61,14 +63,12 @@ export const INITIAL_PRODUCTS: Omit<Product, "id">[] = [
     remaining: 10,
     active: true,
   },
-
   {
     uniqueKey: "8",
-    name: "A la Prochaine",
-    probability: 40,
-    image:
-      "https://imgs.search.brave.com/nsiGlyISticIptpic58hVto5HUTem6f76y5MqA_t5bE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi9mYWls/ZWQtcmVkLXJ1YmJl/ci1zdGFtcC1vdmVy/LXdoaXRlLWJhY2tn/cm91bmQtODg0MTIz/MTQuanBn",
-    remaining: 999999,
+    name: "Produit Mazraa",
+    image: "/images/products/boulettes.png",
+    remaining: 15,
     active: true,
+    probability: 40,
   },
 ];
